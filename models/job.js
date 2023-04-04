cd7da77c08a2e512f4771f4179b4e38bb7d2a6bd
Lexsa Campbell <lexsacampbell@gmail.com>
2023-04-03 19:48:22 -0700

    Added additional methods to Job model

--- models/job.js
+++ models/job.js
@@ -7,21 +7,136 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 /** Related functions for jobs. */
 
 class Job {
-  /** Find all companies.
+  /** Create a job (from data), update db, return new job data.
    *
-   * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
+   * data should be { title, salary, equity, company_handle }
+   *
+   * Returns { id,title, salary, equit, company_handle }
+   *
+   * Throws BadRequestError if job already in database.
+   * */
+
+  static async create({ title, salary, equity, company_handle }) {
+    const duplicateCheck = await db.query(
+          `SELECT id
+           FROM jobs
+           WHERE id = $1`,
+        [id]);
+
+    if (duplicateCheck.rows[0])
+      throw new BadRequestError(`Duplicate company: ${id}`);
+
+    const result = await db.query(
+          `INSERT INTO jobs
+           (title, salary, equity, company_handle)
+           VALUES ($1, $2, $3, $4)
+           RETURNING id, title, salary, equity, company_handle`,
+        [
+          title, 
+          salary, 
+          equity,
+          company_handle
+        ],
+    );
+    const job = result.rows[0];
+
+    return job;
+  }
+
+  /** Find all jobs.
+   *
+   * Returns [{ id, title, salary, equity, company_handle }, ...]
    * */
 
   static async findAll() {
     const jobsRes = await db.query(
           `SELECT id,
-                  title,
+                  title, 
+                  salary, 
+                  equity, 
+                  company_handle
+           FROM jobs
+           ORDER BY id`);
+    return jobsRes.rows;
+  }
+
+  /** Given a job id, return data about job.
+   *
+   * Returns { id, title, salary, equity, company_handle }
+   *
+   * Throws NotFoundError if not found.
+   **/
+
+  static async get(id) {
+    const jobRes = await db.query(
+          `SELECT title,
                   salary,
                   equity,
                   company_handle
            FROM jobs
-           ORDER BY title`);
-    return jobsRes.rows;
+           WHERE id = $1`,
+        [id]);
+
+    const job = jobRes.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+
+  /** Update job data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain all the
+   * fields; this only changes provided ones.
+   *
+   * Data can include: {title, salary, equity}
+   *
+   * Returns {id, title, salary, equity, company_handle}
+   *
+   * Throws NotFoundError if not found.
+   */
+
+  // static async update(id, data) {
+  //   const { setCols, values } = sqlForPartialUpdate(
+  //       data,
+  //       {
+  //         title: "num_employees",
+  //         salary: "salary",
+  //         equity: "equity"
+  //       });
+  //   const handleVarIdx = "$" + (values.length + 1);
+
+  //   const querySql = `UPDATE jobs 
+  //                     SET ${setCols} 
+  //                     WHERE id = ${handleVarIdx} 
+  //                     RETURNING handle, 
+  //                               name, 
+  //                               description, 
+  //                               num_employees AS "numEmployees", 
+  //                               logo_url AS "logoUrl"`;
+  //   const result = await db.query(querySql, [...values, handle]);
+  //   const company = result.rows[0];
+
+  //   if (!company) throw new NotFoundError(`No company: ${handle}`);
+
+  //   return company;
+  // }
+
+  /** Delete given company from database; returns undefined.
+   *
+   * Throws NotFoundError if company not found.
+   **/
+
+  static async remove(id) {
+    const result = await db.query(
+          `DELETE
+           FROM jobs
+           WHERE id = $1
+           RETURNING id`,
+        [id]);
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
   }
 }
 
